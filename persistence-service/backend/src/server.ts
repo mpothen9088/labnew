f19d9105a24faa4a4582a199562e290706b0cc6b
mpothen9088 <124945200+mpothen9088@users.noreply.github.com>
2023-08-09 20:38:36 -0400

    Add files via upload

--- persistence-service/backend/src/server.ts
+++ persistence-service/backend/src/server.ts
@@ -1,23 +1,22 @@
-import cors from "cors";
-import express, { json } from "express";
-import postgresDataSource from "./strategy/postgresql";
-import PhotoApi from "./strategy/postgresql/photo";
-import TypeOrmService from "./persistenceService/typeOrmPersistence/typeOrmPersistence"; // Import the TypeOrmService
+import express from 'express';
+import MockPersistenceService from './persistenceService/typeOrmPersistence/mockTypeOrmService';
+import TypeOrmService from './persistenceService/typeOrmPersistence/typeOrmPersistence';
+import { postgresDataSource } from './strategy/postgresql/configure';
+import PhotoApi from './strategy/postgresql/photo';
 
-(async () => {
-  const app = express();
-  app.use(cors());
-  app.use(json());
+async function startServer() {
+    const app = express();
+    const dataSource = await postgresDataSource.initialize();
+    const typeOrmPersistence = new TypeOrmService(dataSource);
+    const mockedTypeOrmPersistence = new MockPersistenceService();
+    
+    new PhotoApi(mockedTypeOrmPersistence, app);
+    
+    app.get("/", (req, res) => {
+        return res.send("hello from conestoga");
+    });
+    
+    app.listen(8000, () => console.log('express server started'));
+}
 
-  const datasource = await postgresDataSource.initialize();
-  const persistenceService = new TypeOrmService(datasource); // Create an instance of TypeOrmService
-
-  new PhotoApi(persistenceService, app); // Pass the TypeOrmService instance instead of the raw datasource
-  app.get("/", (_, res) => {
-    return res.send("hello world");
-  });
-
-  app.listen(8000, () => {
-    console.log(`express server started on 8000`);
-  });
-})().catch((err) => console.log(err));
+startServer().catch(err => console.error(err));
