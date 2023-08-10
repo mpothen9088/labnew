c3ceda3a6d5eca166e661bf8bcd622a6c1afd93c
mpothen9088 <124945200+mpothen9088@users.noreply.github.com>
2023-08-09 21:24:33 -0400

    Add files via upload

--- persistence-service/backend/src/strategy/postgresql/photo/photoApi.ts
+++ persistence-service/backend/src/strategy/postgresql/photo/photoApi.ts
@@ -1,55 +1,49 @@
 import { Express } from "express";
-import PersistenceService from "../../../persistenceService/persistenceService";
+import { DataSource } from "typeorm";
+import PersistenceService from "../../../persistenceService/persistenceService"; // Fixed the quotation marks
 import { Photo } from "./photo";
 
 export default class PhotoApi {
-  #persistenceService: PersistenceService;
-  #express: Express;
+    #persistenceService: PersistenceService;
+    #express: Express;
 
-  constructor(persistenceService: PersistenceService, express: Express) {
-    this.#persistenceService = persistenceService;
-    this.#express = express;
+    constructor(persistenceService: PersistenceService, express: Express) {
+        this.#persistenceService = persistenceService;
+        this.#express = express;
 
-    this.#express.get("/photo/:id", async (req, res) => {
-      try {
-        const photo = await this.#persistenceService.findBy(Photo, {
-          id: parseInt(req.params.id),
+        this.#express.get("/photo/:id", async (req, res) => {
+            return res.json(
+                await this.#persistenceService.findBy(Photo, { // Removed unnecessary spaces
+                    id: parseInt(req.params.id),
+                })
+            );
         });
-        if (!photo) {
-            return res.status(404).json({ error: "Photo not found" });
-        }
-        return res.json(photo);
-      } catch (err: unknown) {
-        if (err instanceof Error) {
-            console.error(err);
-            return res.status(500).json({ error: "Failed to fetch photo", details: err.message });
-        }
-        return res.status(500).json({ error: "Failed to fetch photo" });
-      }
-    });
 
-    this.#express.post("/photo", async (req, res) => {
-      const { body } = req;
-      console.log(body);
+        this.#express.post("/photo", async (req, res) => {
+            const { body } = req;
+            console.log(body);
 
-      const photo = new Photo();
-      photo.name = body.name;
-      photo.description = body.description;
-      photo.filename = body.filename;
-      photo.views = 0;
-      photo.isPublished = true;
+            const photo = new Photo();
+            photo.name = body.name; // Fixed spacing
+            photo.description = body.description;
+            photo.filename = body.filename;
+            photo.views = 0;
+            photo.isPublished = true;
 
-      try {
-        await this.#persistenceService.insert(photo, "Photo");
-        console.log(`photo has been created with id: ${photo.id}`);
-        return res.status(200).json({ id: photo.id });
-      } catch (err: unknown) {
-        if (err instanceof Error) {
-            console.error(err);
-            return res.status(503).json({ error: "Photo creation failed in db.", details: err.message });
-        }
-        return res.status(503).json({ error: "Photo creation failed in db." });
-      }
-    });
-  }
+            try {
+                await this.#persistenceService.insert(photo, "Photo"); // Fixed quotation marks
+                console.log(`photo has been created with id: ${photo.id}`); // Fixed template string
+            } catch (err) {
+                res.status(503);
+                return res.json({
+                    error: "Photo creation failed in db."
+                });
+            }
+
+            res.status(200);
+            return res.json({
+                id: photo.id,
+            });
+        });
+    }
 }
