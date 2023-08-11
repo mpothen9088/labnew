804334654368f30a022cf727409b9de24cd34452
mpothen9088 <124945200+mpothen9088@users.noreply.github.com>
2023-08-11 10:54:40 -0400

    Update persistenceService.ts

--- persistence-service/backend/src/persistenceService/persistenceService.ts
+++ persistence-service/backend/src/persistenceService/persistenceService.ts
@@ -1,12 +1,9 @@
+// PersistenceService.ts
+
+import { ObjectLiteral } from "typeorm";
+
 export default interface PersistenceService {
-    create: (location: string) => boolean;
-
-    // Option 1: Generalized insert method to accept any type of content
-    insert: <T = unknown>(content: T, location: string) => Promise<boolean>;
-
-    // Uncomment the below line for Option 2, which is specific to Photo type
-    // insert: (content: Photo, location: string) => Promise<boolean>;
-
-    // Assuming you also want to add the findBy method as discussed earlier:
-    findBy: <T = unknown>(entity: { new(): T }, criteria: any) => Promise<T | null>;
+    create(location: string): boolean;
+    insert<T extends ObjectLiteral>(content: T, location: string): Promise<boolean>;
+    findBy<T extends ObjectLiteral>(entity: { new(): T }, criteria: any): Promise<T | null>;
 }
