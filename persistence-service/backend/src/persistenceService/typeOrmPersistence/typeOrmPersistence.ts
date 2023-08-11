da88e7ef29a4b540a3e03183c2b96de96793cff4
mpothen9088 <124945200+mpothen9088@users.noreply.github.com>
2023-08-11 10:47:53 -0400

    Update typeOrmPersistence.ts

--- persistence-service/backend/src/persistenceService/typeOrmPersistence/typeOrmPersistence.ts
+++ persistence-service/backend/src/persistenceService/typeOrmPersistence/typeOrmPersistence.ts
@@ -1,3 +1,5 @@
+// typeOrmPersistence.ts
+
 import { DataSource, ObjectLiteral } from "typeorm";
 import PersistenceService from "../persistenceService";
 
@@ -12,7 +14,6 @@ export default class TypeOrmService implements PersistenceService {
         throw new Error('Not Implemented: TypeOrm does not support this operation');
     }
 
-    // Generalized insert method to accept any type of content
     async insert<T extends ObjectLiteral>(content: T, location: string): Promise<boolean> {
         try {
             await this.#dataSource.manager.save(content);
@@ -22,7 +23,6 @@ export default class TypeOrmService implements PersistenceService {
         }
     }
 
-    // Implemented findBy method using TypeORM's query methods
     async findBy<T extends ObjectLiteral>(entity: { new(): T }, criteria: any): Promise<T | null> {
         try {
             const repository = this.#dataSource.getRepository(entity);
