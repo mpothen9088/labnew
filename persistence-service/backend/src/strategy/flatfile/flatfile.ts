48763d9f28e3aa54adc4aa79144d9d517fdd30e5
mpothen9088 <124945200+mpothen9088@users.noreply.github.com>
2023-08-11 10:15:47 -0400

    Update flatfile.ts

--- persistence-service/backend/src/strategy/flatfile/flatfile.ts
+++ persistence-service/backend/src/strategy/flatfile/flatfile.ts
@@ -14,17 +14,16 @@ export default class FlatfilePersistence implements PersistenceService {
             return false;
         }
 
-        fs.writeFileSync(this.getPath(`flatfileDb`, `${name}.json`), "");
+        fs.writeFileSync(this.getPath(`flatfileDb`, `${name}.json`), "[]");
         return true;
     }
 
     insert<T = unknown>(content: T, location: string): Promise<boolean> {
         return new Promise((resolve, reject) => {
             try {
-                fs.appendFileSync(
-                    this.getPath("flatfileDb", `${location}.json`),
-                    JSON.stringify(content)
-                );
+                const data = JSON.parse(fs.readFileSync(this.getPath("flatfileDb", `${location}.json`), 'utf8'));
+                data.push(content);
+                fs.writeFileSync(this.getPath("flatfileDb", `${location}.json`), JSON.stringify(data));
                 resolve(true);
             } catch (error) {
                 reject(false);
@@ -32,10 +31,21 @@ export default class FlatfilePersistence implements PersistenceService {
         });
     }
 
-    // Modified the findBy method to return null to satisfy the PersistenceService interface
-    async findBy<T = unknown>(_entity: new () => T, _criteria: any): Promise<T | null> {
-        throw new Error("findBy method is not implemented for FlatfilePersistence");
-        return null;  // Explicitly returning null here for consistency
+    async findBy<T = unknown>(_entity: new () => T, criteria: any): Promise<T | null> {
+        try {
+            const data: T[] = JSON.parse(fs.readFileSync(this.getPath("flatfileDb", `${_entity.name}.json`), 'utf8'));
+            const found = data.find(item => {
+                for (let key in criteria) {
+                    if (item[key] !== criteria[key]) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+            return found || null;
+        } catch (error) {
+            throw new Error(`Error reading from flatfile: ${error.message}`);
+        }
     }
 
     private getPath(...dir: string[]) {
