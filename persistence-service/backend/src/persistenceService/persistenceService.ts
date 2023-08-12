2341e480d9dbb8acd542f1f66a5dbf070f7dc9cf
mpothen9088 <124945200+mpothen9088@users.noreply.github.com>
2023-08-12 08:29:03 -0400

    Update persistenceService.ts

--- persistence-service/backend/src/persistenceService/persistenceService.ts
+++ persistence-service/backend/src/persistenceService/persistenceService.ts
@@ -1,9 +1,9 @@
-// PersistenceService.ts
-
 import { ObjectLiteral } from "typeorm";
 
 export default interface PersistenceService {
     create(location: string): boolean;
     insert<T extends ObjectLiteral>(content: T, location: string): Promise<boolean>;
     findBy<T extends ObjectLiteral>(entity: { new(): T }, criteria: any): Promise<T | null>;
+    update<T extends ObjectLiteral>(id: number, content: T): Promise<boolean>;
+    delete<T extends ObjectLiteral>(id: number, entity: { new(): T }): Promise<boolean>;
 }
