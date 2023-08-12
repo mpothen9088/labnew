8a54b7163be7f85c0ce1b556b33a1b50a43a178f
mpothen9088 <124945200+mpothen9088@users.noreply.github.com>
2023-08-12 16:53:41 -0400

    Update driverApi.test.ts

--- persistence-service/backend/src/strategy/postgresql/driver/driverApi.test.ts
+++ persistence-service/backend/src/strategy/postgresql/driver/driverApi.test.ts
@@ -1,86 +1,37 @@
-import { Express } from "express";
-import PersistenceService from "../../../persistenceService/persistenceService";
-import { Driver } from "./driver";
-import MockPersistenceService from "../../../persistenceService/typeOrmPersistence/mockTypeOrmService";
+import express from 'express';
+import request from 'supertest';
+import DriverApi from './driverApi';
+import MockPersistenceService from '../../../persistenceService/typeOrmPersistence/mockTypeOrmService';
 
-export default class DriverApi {
-    #persistenceService: PersistenceService;
-    #express: Express;
+describe('DriverApi CRUD operations', () => {
+    const app = express();
+    app.use(express.json());
+    new DriverApi(new MockPersistenceService(), app);
 
-    constructor(persistenceService: PersistenceService, express: Express) {
-        this.#persistenceService = persistenceService;
-        this.#express = express;
+    it('should create a new driver and return 200', async () => {
+        const driverData = {
+            employee_id: 1,
+            category: 'Heavy Trucks'
+        };
+        const response = await request(app).post('/driver').send(driverData);
+        expect(response.status).toBe(200);
+    });
 
-        // GET route to fetch a driver by its ID
-        this.#express.get("/driver/:id", async (req, res) => {
-            const parsedId = parseInt(req.params.id);
-            const driver = await this.#persistenceService.findBy(Driver, { driver_id: parsedId });
-            if (driver) {
-                return res.json(driver);
-            } else {
-                // Only return a 404 if using the real TypeOrmService
-                if (this.#persistenceService instanceof MockPersistenceService) {
-                    return res.json({ message: 'Using mock service, always returns 200' });
-                } else {
-                    return res.status(404).json({ message: 'Driver not found' });
-                }
-            }
-        });
+    it('should return 200 when fetching a driver by its ID', async () => {
+        const response = await request(app).get('/driver/1');
+        expect(response.status).toBe(200);
+    });
 
-        // POST route to create a new driver
-        this.#express.post("/driver", async (req, res) => {
-            const { body } = req;
+    it('should update a driver by its ID and return 200', async () => {
+        const updatedData = {
+            category: 'Light Trucks'
+        };
+        const response = await request(app).put('/driver/1').send(updatedData);
+        expect(response.status).toBe(200);
+    });
 
-            const driver = new Driver();
-            driver.employee_id = body.employee_id;
-            driver.category = body.category;
-
-            try {
-                await this.#persistenceService.insert(driver, "Driver");
-                console.log(`Driver has been created with id: ${driver.driver_id}`);
-                res.status(200);
-                return res.json({
-                    driver_id: driver.driver_id,
-                });
-            } catch (err) {
-                res.status(503);
-                return res.json({
-                    error: "Driver creation failed in db."
-                });
-            }
-        });
-
-        // PUT route to update a driver by its ID
-        this.#express.put("/driver/:id", async (req, res) => {
-            const parsedId = parseInt(req.params.id);
-            const updatedDriver = req.body;
-
-            try {
-                const success = await this.#persistenceService.update(parsedId, updatedDriver);
-                if (success) {
-                    return res.json({ message: 'Driver updated successfully' });
-                } else {
-                    return res.status(404).json({ message: 'Driver not found' });
-                }
-            } catch (err) {
-                return res.status(503).json({ error: "Driver update failed in db." });
-            }
-        });
-
-        // DELETE route to delete a driver by its ID
-        this.#express.delete("/driver/:id", async (req, res) => {
-            const parsedId = parseInt(req.params.id);
-
-            try {
-                const success = await this.#persistenceService.delete(parsedId, Driver);
-                if (success) {
-                    return res.json({ message: 'Driver deleted successfully' });
-                } else {
-                    return res.status(404).json({ message: 'Driver not found' });
-                }
-            } catch (err) {
-                return res.status(503).json({ error: "Driver deletion failed in db." });
-            }
-        });
-    }
-}
+    it('should delete a driver by its ID and return 200', async () => {
+        const response = await request(app).delete('/driver/1');
+        expect(response.status).toBe(200);
+    });
+});
