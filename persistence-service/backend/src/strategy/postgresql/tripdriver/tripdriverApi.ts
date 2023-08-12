d6cab8b74729f6e09db4205f11210996cbc01652
mpothen9088 <124945200+mpothen9088@users.noreply.github.com>
2023-08-12 17:35:49 -0400

    Update tripdriverApi.ts

--- persistence-service/backend/src/strategy/postgresql/tripdriver/tripdriverApi.ts
+++ persistence-service/backend/src/strategy/postgresql/tripdriver/tripdriverApi.ts
@@ -1,9 +1,9 @@
 import { Express } from "express";
 import PersistenceService from "../../../persistenceService/persistenceService";
-import { Truck } from "./truck";
+import { TripDriver } from "./tripdriver";
 import MockPersistenceService from "../../../persistenceService/typeOrmPersistence/mockTypeOrmService";
 
-export default class TruckApi {
+export default class TripDriverApi {
     #persistenceService: PersistenceService;
     #express: Express;
 
@@ -11,78 +11,76 @@ export default class TruckApi {
         this.#persistenceService = persistenceService;
         this.#express = express;
 
-        // GET route to fetch a truck by its ID
-        this.#express.get("/truck/:id", async (req, res) => {
-            const parsedId = parseInt(req.params.id);
-            const truck = await this.#persistenceService.findBy(Truck, { truck_id: parsedId });
-            if (truck) {
-                return res.json(truck);
+        // GET route to fetch a tripdriver by its trip ID
+        this.#express.get("/tripdriver/:trip_id", async (req, res) => {
+            const parsedId = parseInt(req.params.trip_id);
+            const tripDriver = await this.#persistenceService.findBy(TripDriver, { trip_id: parsedId });
+            if (tripDriver) {
+                return res.json(tripDriver);
             } else {
                 // Only return a 404 if using the real TypeOrmService
                 if (this.#persistenceService instanceof MockPersistenceService) {
                     return res.json({ message: 'Using mock service, always returns 200' });
                 } else {
-                    return res.status(404).json({ message: 'Truck not found' });
+                    return res.status(404).json({ message: 'TripDriver not found' });
                 }
             }
         });
 
-        // POST route to create a new truck
-        this.#express.post("/truck", async (req, res) => {
+        // POST route to create a new tripdriver
+        this.#express.post("/tripdriver", async (req, res) => {
             const { body } = req;
 
-            const truck = new Truck();
-            truck.brand = body.brand;
-            truck.load = body.load;
-            truck.capacity = body.capacity;
-            truck.year = body.year;
-            truck.number_of_repairs = body.number_of_repairs;
+            const tripDriver = new TripDriver();
+            tripDriver.trip_id = body.trip_id;
+            tripDriver.driver_id = body.driver_id;
 
             try {
-                await this.#persistenceService.insert(truck, "Truck");
-                console.log(`Truck has been created with id: ${truck.truck_id}`);
+                await this.#persistenceService.insert(tripDriver, "TripDriver");
+                console.log(`TripDriver has been created with trip_id: ${tripDriver.trip_id}`);
                 res.status(200);
                 return res.json({
-                    truck_id: truck.truck_id,
+                    trip_id: tripDriver.trip_id,
+                    driver_id: tripDriver.driver_id
                 });
             } catch (err) {
                 res.status(503);
                 return res.json({
-                    error: "Truck creation failed in db."
+                    error: "TripDriver creation failed in db."
                 });
             }
         });
 
-        // PUT route to update a truck by its ID
-        this.#express.put("/truck/:id", async (req, res) => {
-            const parsedId = parseInt(req.params.id);
-            const updatedTruck = req.body;
+        // PUT route to update a tripdriver by its trip ID
+        this.#express.put("/tripdriver/:trip_id", async (req, res) => {
+            const parsedId = parseInt(req.params.trip_id);
+            const updatedTripDriver = req.body;
 
             try {
-                const success = await this.#persistenceService.update(parsedId, updatedTruck);
+                const success = await this.#persistenceService.update(parsedId, updatedTripDriver);
                 if (success) {
-                    return res.json({ message: 'Truck updated successfully' });
+                    return res.json({ message: 'TripDriver updated successfully' });
                 } else {
-                    return res.status(404).json({ message: 'Truck not found' });
+                    return res.status(404).json({ message: 'TripDriver not found' });
                 }
             } catch (err) {
-                return res.status(503).json({ error: "Truck update failed in db." });
+                return res.status(503).json({ error: "TripDriver update failed in db." });
             }
         });
 
-        // DELETE route to delete a truck by its ID
-        this.#express.delete("/truck/:id", async (req, res) => {
-            const parsedId = parseInt(req.params.id);
+        // DELETE route to delete a tripdriver by its trip ID
+        this.#express.delete("/tripdriver/:trip_id", async (req, res) => {
+            const parsedId = parseInt(req.params.trip_id);
 
             try {
-                const success = await this.#persistenceService.delete(parsedId, Truck);
+                const success = await this.#persistenceService.delete(parsedId, TripDriver);
                 if (success) {
-                    return res.json({ message: 'Truck deleted successfully' });
+                    return res.json({ message: 'TripDriver deleted successfully' });
                 } else {
-                    return res.status(404).json({ message: 'Truck not found' });
+                    return res.status(404).json({ message: 'TripDriver not found' });
                 }
             } catch (err) {
-                return res.status(503).json({ error: "Truck deletion failed in db." });
+                return res.status(503).json({ error: "TripDriver deletion failed in db." });
             }
         });
     }
