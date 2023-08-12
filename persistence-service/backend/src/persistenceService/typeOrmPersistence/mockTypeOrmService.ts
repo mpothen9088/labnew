51df0d7d2ce02257a7ceb94da64b18cbb42116f6
mpothen9088 <124945200+mpothen9088@users.noreply.github.com>
2023-08-12 08:28:03 -0400

    Update mockTypeOrmService.ts

--- persistence-service/backend/src/persistenceService/typeOrmPersistence/mockTypeOrmService.ts
+++ persistence-service/backend/src/persistenceService/typeOrmPersistence/mockTypeOrmService.ts
@@ -8,11 +8,25 @@ export default class MockPersistenceService implements PersistenceService {
 
   async insert<T = unknown>(content: T, location: string): Promise<boolean> {
     // Mock implementation for insert
+    console.log(`Mock: Inserted into ${location}`);
     return true;
   }
 
   async findBy<T = unknown>(entity: new () => T, criteria: any): Promise<T | null> {
     // Mock implementation for findBy
+    console.log(`Mock: Find by criteria in ${entity.name}`);
     return null;
+  }
+
+  async update<T = unknown>(id: number, content: T): Promise<boolean> {
+    // Mock implementation for update
+    console.log(`Mock: Updated record with ID ${id}`);
+    return true;
+  }
+
+  async delete<T = unknown>(id: number, entity: new () => T): Promise<boolean> {
+    // Mock implementation for delete
+    console.log(`Mock: Deleted record with ID ${id} from ${entity.name}`);
+    return true;
   }
 }
