9a212838712f083e07261eb5cb9679ee82e342a5
mpothen9088 <124945200+mpothen9088@users.noreply.github.com>
2023-08-12 16:52:25 -0400

    Update mechanicApi.test.ts

--- persistence-service/backend/src/strategy/postgresql/mechanic/mechanicApi.test.ts
+++ persistence-service/backend/src/strategy/postgresql/mechanic/mechanicApi.test.ts
@@ -1,84 +1,37 @@
-import { Express } from "express";
-import PersistenceService from "../../../persistenceService/persistenceService";
-import { Mechanic } from "./mechanic";
-import MockPersistenceService from "../../../persistenceService/typeOrmPersistence/mockTypeOrmService";
+import express from 'express';
+import request from 'supertest';
+import MechanicApi from './mechanicApi';
+import MockPersistenceService from '../../../persistenceService/typeOrmPersistence/mockTypeOrmService';
 
-export default class MechanicApi {
-    #persistenceService: PersistenceService;
-    #express: Express;
+describe('MechanicApi CRUD operations', () => {
+    const app = express();
+    app.use(express.json());
+    new MechanicApi(new MockPersistenceService(), app);
 
-    constructor(persistenceService: PersistenceService, express: Express) {
-        this.#persistenceService = persistenceService;
-        this.#express = express;
+    it('should create a new mechanic and return 200', async () => {
+        const mechanicData = {
+            employee_id: 1,
+            specialized_brand: 'BrandX'
+        };
+        const response = await request(app).post('/mechanic').send(mechanicData);
+        expect(response.status).toBe(200);
+    });
 
-        // GET route to fetch a mechanic by its ID
-        this.#express.get("/mechanic/:id", async (req, res) => {
-            const parsedId = parseInt(req.params.id);
-            const mechanic = await this.#persistenceService.findBy(Mechanic, { employee_id: parsedId });
-            if (mechanic) {
-                return res.json(mechanic);
-            } else {
-                if (this.#persistenceService instanceof MockPersistenceService) {
-                    return res.json({ message: 'Using mock service, always returns 200' });
-                } else {
-                    return res.status(404).json({ message: 'Mechanic not found' });
-                }
-            }
-        });
+    it('should return 200 when fetching a mechanic by its ID', async () => {
+        const response = await request(app).get('/mechanic/1');
+        expect(response.status).toBe(200);
+    });
 
-        // POST route to create a new mechanic
-        this.#express.post("/mechanic", async (req, res) => {
-            const { body } = req;
+    it('should update a mechanic by its ID and return 200', async () => {
+        const updatedData = {
+            specialized_brand: 'BrandY'
+        };
+        const response = await request(app).put('/mechanic/1').send(updatedData);
+        expect(response.status).toBe(200);
+    });
 
-            const mechanic = new Mechanic();
-            mechanic.specialized_brand = body.specialized_brand;
-
-            try {
-                await this.#persistenceService.insert(mechanic, "Mechanic");
-                console.log(`Mechanic has been created with id: ${mechanic.employee_id}`);
-                res.status(200);
-                return res.json({
-                    employee_id: mechanic.employee_id,
-                });
-            } catch (err) {
-                res.status(503);
-                return res.json({
-                    error: "Mechanic creation failed in db."
-                });
-            }
-        });
-
-        // PUT route to update a mechanic by its ID
-        this.#express.put("/mechanic/:id", async (req, res) => {
-            const parsedId = parseInt(req.params.id);
-            const updatedMechanic = req.body;
-
-            try {
-                const success = await this.#persistenceService.update(parsedId, updatedMechanic);
-                if (success) {
-                    return res.json({ message: 'Mechanic updated successfully' });
-                } else {
-                    return res.status(404).json({ message: 'Mechanic not found' });
-                }
-            } catch (err) {
-                return res.status(503).json({ error: "Mechanic update failed in db." });
-            }
-        });
-
-        // DELETE route to delete a mechanic by its ID
-        this.#express.delete("/mechanic/:id", async (req, res) => {
-            const parsedId = parseInt(req.params.id);
-
-            try {
-                const success = await this.#persistenceService.delete(parsedId, Mechanic);
-                if (success) {
-                    return res.json({ message: 'Mechanic deleted successfully' });
-                } else {
-                    return res.status(404).json({ message: 'Mechanic not found' });
-                }
-            } catch (err) {
-                return res.status(503).json({ error: "Mechanic deletion failed in db." });
-            }
-        });
-    }
-}
+    it('should delete a mechanic by its ID and return 200', async () => {
+        const response = await request(app).delete('/mechanic/1');
+        expect(response.status).toBe(200);
+    });
+});
