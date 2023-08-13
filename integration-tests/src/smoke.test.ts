895b3a378781e699d2f6a29b283fd346a937fb3f
mpothen9088 <124945200+mpothen9088@users.noreply.github.com>
2023-08-13 11:28:58 -0400

    Update smoke.test.ts

--- integration-tests/src/smoke.test.ts
+++ integration-tests/src/smoke.test.ts
@@ -4,10 +4,14 @@ const targetUrl = `${process.env.TARGET_URL}`;
 
 describe("smoke", () => {
   it("persistence-service status is 200", async () => {
-    console.log(targetUrl);
-    const health = await axios.get(`http://${targetUrl}`);
-
-    console.log(health.status);
-    expect(health.status).toBe(200);
+    console.log(`Accessing target URL: ${targetUrl}`);
+    
+    const response = await axios.get(`http://${targetUrl}`);
+    
+    console.log(`Received status: ${response.status}`);
+    console.log(`Received data: ${response.data}`);
+    
+    expect(response.status).toBe(200);
+    expect(response.data).toBe("Road Freight Transportation company");
   });
 });
