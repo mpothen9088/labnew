0dca0b3721d78a65ac4950083fa5e8dc9418c20a
mpothen9088 <124945200+mpothen9088@users.noreply.github.com>
2023-08-12 22:34:29 -0400

    Update smoke.test.ts

--- integration-tests/src/smoke.test.ts
+++ integration-tests/src/smoke.test.ts
@@ -1,13 +1,40 @@
-import axios from "axios";
+import request from 'supertest';
 
-const targetUrl = `${process.env.TARGET_URL}`;
+const baseURL = 'http://localhost:80';
 
-describe("smoke", () => {
-  it("persistence-service status is 200", async () => {
-    console.log(targetUrl);
-    const health = await axios.get(`http://${targetUrl}`);
+describe('Integration tests for /truck API', () => {
+    let createdTruckId: number;
 
-    console.log(health.status);
-    expect(health.status).toBe(200);
-  });
+    it('should create a new truck and return 200', async () => {
+        const truckData = {
+            brand: 'Test Truck Brand',
+            load: 500,
+            capacity: 1500,
+            year: 2022,
+            number_of_repairs: 2
+        };
+        const response = await request(baseURL).post('/truck').send(truckData);
+        expect(response.status).toBe(200);
+        expect(response.body.truck_id).toBeDefined();
+        createdTruckId = response.body.truck_id;
+    });
+
+    it('should fetch the created truck by its ID and return 200', async () => {
+        const response = await request(baseURL).get(`/truck/${createdTruckId}`);
+        expect(response.status).toBe(200);
+        expect(response.body.brand).toBe('Test Truck Brand');
+    });
+
+    it('should update the created truck by its ID and return 200', async () => {
+        const updatedData = {
+            brand: 'Updated Truck Brand'
+        };
+        const response = await request(baseURL).put(`/truck/${createdTruckId}`).send(updatedData);
+        expect(response.status).toBe(200);
+    });
+
+    it('should delete the truck by its ID and return 200', async () => {
+        const response = await request(baseURL).delete(`/truck/${createdTruckId}`);
+        expect(response.status).toBe(200);
+    });
 });
