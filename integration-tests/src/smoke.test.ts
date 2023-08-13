b7ba6e0879005424b29a94b82e2f2295955cec23
mpothen9088 <124945200+mpothen9088@users.noreply.github.com>
2023-08-13 13:05:23 -0400

    Add files via upload

--- integration-tests/src/smoke.test.ts
+++ integration-tests/src/smoke.test.ts
@@ -1,40 +1,13 @@
-import request from 'supertest';
+import axios from "axios";
 
-const baseURL = 'http://localhost:80';
+const targetUrl = `${process.env.TARGET_URL}`;
 
-describe('Integration tests for /truck API', () => {
-    let createdTruckId: number;
+describe("smoke", () => {
+  it("persistence-service status is 200", async () => {
+    console.log(targetUrl);
+    const health = await axios.get(`http://${targetUrl}`);
 
-    it('should create a new truck and return 200', async () => {
-        const truckData = {
-            brand: 'Test Truck Brand',
-            load: 500,
-            capacity: 1500,
-            year: 2022,
-            number_of_repairs: 2
-        };
-        const response = await request(baseURL).post('/truck').send(truckData);
-        expect(response.status).toBe(200);
-        expect(response.body.truck_id).toBeDefined();
-        createdTruckId = response.body.truck_id;
-    });
-
-    it('should fetch the created truck by its ID and return 200', async () => {
-        const response = await request(baseURL).get(`/truck/${createdTruckId}`);
-        expect(response.status).toBe(200);
-        expect(response.body.brand).toBe('Test Truck Brand');
-    });
-
-    it('should update the created truck by its ID and return 200', async () => {
-        const updatedData = {
-            brand: 'Updated Truck Brand'
-        };
-        const response = await request(baseURL).put(`/truck/${createdTruckId}`).send(updatedData);
-        expect(response.status).toBe(200);
-    });
-
-    it('should delete the truck by its ID and return 200', async () => {
-        const response = await request(baseURL).delete(`/truck/${createdTruckId}`);
-        expect(response.status).toBe(200);
-    });
+    console.log(health.status);
+    expect(health.status).toBe(200);
+  });
 });
